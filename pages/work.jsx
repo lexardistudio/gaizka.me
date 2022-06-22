e522d2dd8cff875a282881cc59dfce19806f8b26
Lexardi Studio <lexardi.studio@gmail.com>
2022-06-22 12:04:04 +0200

    rama dev

--- pages/work.jsx
+++
@@ -1,9 +0,0 @@
-export default function Work() {
-    return (
-        <>
-            <div className="flex items-center border-2 border-gray900">
-                <h1>prueba</h1>
-            </div>
-        </>
-    )
-}
